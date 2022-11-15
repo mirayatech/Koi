e44020433a857926069c8f99769bfe9cca0aa24d
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-15 21:51:23 +0100

    style: general and global styling

---
+++ styles/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  White: '#fff',
+  Blue: '#2563EB',
+  Navy_Blue: '#0f172a',
+  Slate_Blue: '#1e293b',
+  Tag_Blue: '#334155',
+  Green: '#18bc9c',
+  Red: '#ec4a57',
+  Yellow: '#f39c13',
+}
