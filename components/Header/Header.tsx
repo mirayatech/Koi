1e0db2fc53b6c13db88b41c27d466cd86ba02ca4
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-16 06:50:45 +0100

    feat: create hamburger menu

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,22 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { HeaderComponent } from './style'
+
+import styles from '../../styles/Component.module.css'
+import ClickAwayListener from 'react-click-away-listener'
+
 import { IoSearchOutline } from 'react-icons/io5'
+import { HiBars3, HiOutlineXMark } from 'react-icons/hi2'
+import { HeaderComponent, ToggleButton, Hamburger, Menu } from './style'
+
+import { useState } from 'react'
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const handleOnClick = () => {
+    setIsMenuOpen(false)
+  }
+
   return (
     <HeaderComponent>
       <Link href="/">
@@ -11,9 +24,34 @@ export default function Header() {
         <span>KOI</span>
       </Link>
 
-      <Link href="/search" aria-label="search">
+      <Link
+        href="/search"
+        aria-label="search"
+        className={styles.header__search}
+      >
         <IoSearchOutline />
       </Link>
+      <Hamburger>
+        <ToggleButton onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          {isMenuOpen ? <HiOutlineXMark /> : <HiBars3 />}
+        </ToggleButton>
+
+        {isMenuOpen && (
+          <ClickAwayListener onClickAway={() => setIsMenuOpen(false)}>
+            <Menu>
+              <Link href="/" onClick={handleOnClick}>
+                Current Season
+              </Link>
+              <Link href="/top" onClick={handleOnClick}>
+                Top Anime
+              </Link>
+              <Link href="/search" onClick={handleOnClick}>
+                Search
+              </Link>
+            </Menu>
+          </ClickAwayListener>
+        )}
+      </Hamburger>
     </HeaderComponent>
   )
 }
