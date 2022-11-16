c8f5580098ae7ece2935a6f5a963782a19fec9f9
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-16 06:14:04 +0100

    style: add styling to app

---
+++ components/Layout/style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+  margin: auto;
+  display: flex;
+  max-width: 1300px;
+  justify-content: center;
+  flex-direction: column;
+`
