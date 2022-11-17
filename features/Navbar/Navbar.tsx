a89b72e34482e8aa362e67005abaad225fc5edb8
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-17 20:42:01 +0100

    feat: create navbar

---
+++ features/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import styles from './Navbar.module.css'
+import { Nav } from './style'
+
+export function Navbar() {
+  const router = useRouter()
+  return (
+    <Nav>
+      <Link href="/" className={router.pathname == '/' ? styles.active : ''}>
+        Current Season
+      </Link>
+      <Link
+        href="/top"
+        className={router.pathname == '/top' ? styles.active : ''}
+      >
+        Top Anime
+      </Link>
+    </Nav>
+  )
+}
