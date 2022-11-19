517e8805d16c1cfd871addef3162fd2b2e1065ed
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-19 11:19:54 +0100

    FEATURE fetch current season animes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
+/* eslint-disable react/jsx-key */
 import Head from 'next/head'
+import { AnimeCard } from '../features'
+import { AnimeType, CURRENT_SEASON_URL } from '../library'
+import styles from '../styles/Home.module.css'
+export type HomeProps = {
+  animes: AnimeType[]
+}
 
-export default function index() {
+export default function Home({ animes }: HomeProps) {
   return (
-    <div>
+    <div className={styles.grid}>
       <Head>
         <title>Koi</title>
         <meta
@@ -11,6 +18,21 @@ export default function index() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {animes.map((anime) => (
+        <AnimeCard anime={anime} key={anime.mal_id} />
+      ))}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(CURRENT_SEASON_URL)
+  const animes = await res.json()
+
+  return {
+    props: {
+      animes: animes.data,
+    },
+  }
 }
