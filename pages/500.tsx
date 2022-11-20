fb97152fee3a7fe8cdd6019739365ab1976adc8b
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-20 09:04:50 +0100

    FEAT: 500 side

---
+++ pages/500.tsx
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled'
+import Image from 'next/image'
+import Link from 'next/link'
+import { theme } from '../styles/theme'
+
+export default function NotFound() {
+  return (
+    <Container>
+      <Card>
+        <Image src="/demon-slayer.gif" alt="" width={100} height={100} />
+
+        <h1>500 | Internal Server Error.</h1>
+        <Link href="/">Go Home</Link>
+      </Card>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Card = styled.div`
+  padding: 30px;
+  display: flex;
+  border-radius: 4px;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+  img {
+    margin-bottom: 20px;
+  }
+
+  h1 {
+    font-weight: 500;
+    font-size: 1.5rem;
+    text-align: center;
+    margin-bottom: 20px;
+    color: ${theme.White};
+  }
+
+  a {
+    border-radius: 2px;
+    padding: 5px 10px;
+    color: ${theme.White};
+    background-color: ${theme.Blue};
+  }
+`
